e4bdcdbbbc242deccd9092381e25799c9b848206
tjrgus123 <korgatpa@gmail.com>
2021-09-01 16:06:08 +0900

    EnemyTracking

--- ProjectGMUnity/ProjectGMDiscord/Assets/Scripts/PlayerCharaterTurretMove.cs
+++ ProjectGMUnity/ProjectGMDiscord/Assets/Scripts/PlayerCharaterTurretMove.cs
@@ -6,9 +6,19 @@ public class PlayerCharaterTurretMove : MonoBehaviour
 {
     public GameObject mainTurret;
 
+    //적 오브젝트 오직 실험용
+    public GameObject enemyObject;
 
     void Update()
     {
-        
+        TurretRotation();
+    }
+
+    public void TurretRotation()
+    {
+        Vector3 enemyPosition = new Vector3(enemyObject.transform.position.x - transform.position.x, 0f, enemyObject.transform.position.z - transform.position.z);
+
+        Quaternion turretRotation = Quaternion.LookRotation(enemyPosition);
+        transform.rotation = turretRotation;
     }
 }
